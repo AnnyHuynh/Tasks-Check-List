572952a75939e4e642b38a2af13b4f300552fd72
AnnyHuynh <anny_huynh32@yahoo.com>
2019-04-03 23:46:43 -0700

    updated

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const taskRoutes = express.Router();
 // const taskController = require('./controllers/taskController');
 const PORT = process.env.PORT || 3002;
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 app.use(cors());
 
 // app.use('/task', taskController);
