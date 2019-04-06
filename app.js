5c3b8826fe37d7bd736ad39bf434cb3047ff6f50
AnnyHuynh <anny_huynh32@yahoo.com>
2019-04-05 21:10:20 -0700

    updated

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ var path = require('path');
 
 app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 // app.use('/task', taskController);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')))
